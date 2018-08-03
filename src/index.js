c4c3d8c16a942b8658d62a773946209c34a7928f
Cookienat0r <nalesencova@gmail.com>
2018-08-02 22:16:38 -0400

    tedious svg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import FlagSVG from './Flag.svg';
-// import './index.css';
+import './index.css';
 
 const Header = () => {
   return (
@@ -14,82 +13,143 @@ const Header = () => {
 const Flag = () => {
   return (
     <div>
-      {/* <img src={FlagSVG} width='600' height='400' /> */}
-      <svg xmlns="http://www.w3.org/2000/svg" width="1235"
+      <svg class="flag" xmlns="http://www.w3.org/2000/svg" width="1235"
       xmlnsLink="http://www.w3.org/1999/xlink" height="650" fill="#FFF">
         <path d="m0,0h1235v650H0"></path>
         <path stroke="#B22234" strokeWidth="2470" strokeDasharray="50" d="m0,0v651"></path>
         <path fill="#3C3B6E" d="m0,0h494v350H0"></path>
-        <g id="q"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="q"></path></g>
-        <g id="d"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="d"></path></g>
-        <g id="e"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="e"></path></g>
-        <g id="f"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="f"></path></g>
-        <g id="t"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="t"></path></g>
-        <g id="w"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="w"></path></g>
-        <g id="a"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="a"></path></g>
-        <g id="b"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="b"></path></g>
-        <g id="c"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="c"></path></g>
-        <g class="fill" id="s"><path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="s"></path></g>
-        
-        <path d="m30.1,50.6 12-36 12,36-30.8-22h37.8" id="s"></path>
-      {/* line 1 */}
-        <use xlinkHref="#s" x="82"></use>
-        <use xlinkHref="#s" x="164"></use>
-        <use xlinkHref="#s" x="250"></use>
-        <use xlinkHref="#s" x="328"></use>
-        <use xlinkHref="#s" x="410"></use>
+        <g class="line1">
+          <path id="star0" d="m0,0 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star1" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star2" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star3" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star4" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star5" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line2">
+          <path id="star6" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star7" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star8" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star9" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star10" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line3">
+          <path id="star11" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star12" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star13" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star14" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star15" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star16" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line4">
+          <path id="star17" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star18" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star19" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star20" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star21" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+
+        </g>
+        <g class="line5">
+          <path id="star22" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star23" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star24" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star25" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star26" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star27" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line6">
+          <path id="star28" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star29" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star30" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star31" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star32" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line7">
+          <path id="star33" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star34" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star35" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star36" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star37" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star38" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line8">
+          <path id="star39" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star40" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star41" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star42" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star43" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+        <g class="line9">
+          <path id="star44" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star45" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star46" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star47" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star48" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+          <path id="star49" d="m30.1,50.6 12-36 12,36-30.8-22h37.8"></path>
+        </g>
+
+{/* try to position all stars in their <path>, use m. 
+  in order to remove everything below */}
+
+      {/* line 1 
+        <use xlinkHref="#star0" x="0"></use>*/}
+        <use xlinkHref="#star1" x="82"></use>
+        <use xlinkHref="#star2" x="164"></use>
+        <use xlinkHref="#star3" x="250"></use>
+        <use xlinkHref="#star4" x="328"></use>
+        <use xlinkHref="#star5" x="410"></use>
       {/* line 2 */}
-        <use xlinkHref="#a" x="41" y="35"></use>
-        <use xlinkHref="#a" x="124" y="35"></use>
-        <use xlinkHref="#a" x="206" y="35"></use>
-        <use xlinkHref="#a" x="286" y="35"></use>
-        <use xlinkHref="#a" x="370" y="35"></use>     
+        <use xlinkHref="#star6" x="41" y="35"></use>
+        <use xlinkHref="#star7" x="124" y="35"></use>
+        <use xlinkHref="#star8" x="206" y="35"></use>
+        <use xlinkHref="#star9" x="286" y="35"></use>
+        <use xlinkHref="#star10" x="370" y="35"></use>     
       {/* line 3 */}
-        <use xlinkHref="#d" y="70"></use>
-        <use xlinkHref="#d" x="82" y="70"></use>
-        <use xlinkHref="#d" x="164" y="70"></use>
-        <use xlinkHref="#d" x="250" y="70"></use>
-        <use xlinkHref="#d" x="328" y="70"></use>
-        <use xlinkHref="#d" x="410" y="70"></use>
+        <use xlinkHref="#star11" y="70"></use>
+        <use xlinkHref="#star12" x="82" y="70"></use>
+        <use xlinkHref="#star13" x="164" y="70"></use>
+        <use xlinkHref="#star14" x="250" y="70"></use>
+        <use xlinkHref="#star15" x="328" y="70"></use>
+        <use xlinkHref="#star16" x="410" y="70"></use>
       {/* line 4 */}
-        <use xlinkHref="#b" x="41" y="105"></use>
-        <use xlinkHref="#b" x="124" y="105"></use>
-        <use xlinkHref="#b" x="206" y="105"></use>
-        <use xlinkHref="#b" x="286" y="105"></use>
-        <use xlinkHref="#b" x="370" y="105"></use>
+        <use xlinkHref="#star17" x="41" y="105"></use>
+        <use xlinkHref="#star18" x="124" y="105"></use>
+        <use xlinkHref="#star19" x="206" y="105"></use>
+        <use xlinkHref="#star20" x="286" y="105"></use>
+        <use xlinkHref="#star21" x="370" y="105"></use>
       {/* line 5 */}
-        <use xlinkHref="#q" y="140"></use>
-        <use xlinkHref="#q" x="82" y="140"></use>
-        <use xlinkHref="#q" x="164" y="140"></use>
-        <use xlinkHref="#q" x="250" y="140"></use>
-        <use xlinkHref="#q" x="328" y="140"></use>
-        <use xlinkHref="#q" x="410" y="140"></use>
+        <use xlinkHref="#star22" y="140"></use>
+        <use xlinkHref="#star23" x="82" y="140"></use>
+        <use xlinkHref="#star24" x="164" y="140"></use>
+        <use xlinkHref="#star25" x="250" y="140"></use>
+        <use xlinkHref="#star26" x="328" y="140"></use>
+        <use xlinkHref="#star27" x="410" y="140"></use>
       {/* line 6 */}
-        <use xlinkHref="#c" x="41" y="175"></use>
-        <use xlinkHref="#c" x="124" y="175"></use>
-        <use xlinkHref="#c" x="206" y="175"></use>
-        <use xlinkHref="#c" x="286" y="175"></use>
-        <use xlinkHref="#c" x="370" y="175"></use>
+        <use xlinkHref="#star28" x="41" y="175"></use>
+        <use xlinkHref="#star29" x="124" y="175"></use>
+        <use xlinkHref="#star30" x="206" y="175"></use>
+        <use xlinkHref="#star31" x="286" y="175"></use>
+        <use xlinkHref="#star32" x="370" y="175"></use>
       {/* line 7 */}
-        <use xlinkHref="#w" y="210"></use>
-        <use xlinkHref="#q" x="82" y="210"></use>
-        <use xlinkHref="#q" x="164" y="210"></use>
-        <use xlinkHref="#q" x="250" y="210"></use>
-        <use xlinkHref="#q" x="328" y="210"></use>
-        <use xlinkHref="#q" x="410" y="210"></use>
+        <use xlinkHref="#star33" y="210"></use>
+        <use xlinkHref="#star34" x="82" y="210"></use>
+        <use xlinkHref="#star35" x="164" y="210"></use>
+        <use xlinkHref="#star36" x="250" y="210"></use>
+        <use xlinkHref="#star37" x="328" y="210"></use>
+        <use xlinkHref="#star38" x="410" y="210"></use>
       {/* line 8 */}
-        <use xlinkHref="#f" x="41" y="246"></use>
-        <use xlinkHref="#f" x="124" y="246"></use>
-        <use xlinkHref="#f" x="206" y="246"></use>
-        <use xlinkHref="#f" x="286" y="246"></use>
-        <use xlinkHref="#f" x="370" y="246"></use>
+        <use xlinkHref="#star39" x="41" y="246"></use>
+        <use xlinkHref="#star40" x="124" y="246"></use>
+        <use xlinkHref="#star41" x="206" y="246"></use>
+        <use xlinkHref="#star42" x="286" y="246"></use>
+        <use xlinkHref="#star43" x="370" y="246"></use>
       {/* line 9 */}
-        <use xlinkHref="#e" y="280"></use>
-        <use xlinkHref="#e" x="82" y="280"></use>
-        <use xlinkHref="#e" x="164" y="280"></use>
-        <use xlinkHref="#e" x="250" y="280"></use>
-        <use xlinkHref="#e" x="328" y="280"></use>
-        <use xlinkHref="#e" x="410" y="280"></use>
+        <use xlinkHref="#star44" y="280"></use>
+        <use xlinkHref="#star45" x="82" y="280"></use>
+        <use xlinkHref="#star46" x="164" y="280"></use>
+        <use xlinkHref="#star47" x="250" y="280"></use>
+        <use xlinkHref="#star48" x="328" y="280"></use>
+        <use xlinkHref="#star49" x="410" y="280"></use>
       </svg>
     </div>
   );
